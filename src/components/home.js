8d688e4e00079837e5e5cb2270f217df3ccd3fe1
Matt Farnsworth <mryanfarnsworth@gmail.com>
2018-07-02 15:23:04 -0600

    app setup

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h1>Course Scheduler</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
