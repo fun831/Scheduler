019ef46f06298b68af88585aa9eb5848d40bae07
Matt Farnsworth <mryanfarnsworth@gmail.com>
2018-07-03 13:30:58 -0600

    Mapped couse object data into library couse component instances.

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -13,8 +13,8 @@ class Library extends Component {
 
     renderCourses() {
         const data = this.props.courses
-        return data.map(course, index => {
-            return <LibraryCourse key={index}/>
+        return data.map((course, index) => {
+            return <LibraryCourse {...course} key={index}/>
         })
     }
 
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -8,19 +8,14 @@ class LibraryCourse extends Component {
         return (
             <div className="library-course">
             <div className='library-course__title-check'>
-                <label className="library-course">Problem Solving</label>
+                <label className="library-course">{this.props.title}</label>
                 { Icon('fas fa-check', 'library-course__icon') }
             </div>
             <Arrow className="library-course__arrow" />
             <Action className="library-course__action" />
             <div className="library-course__description">
                 <label>Couse Description</label>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type 
-                    specimen book. 
-                </p>
+                <p>{this.props.description}</p>
             </div>
             </div>
         );
