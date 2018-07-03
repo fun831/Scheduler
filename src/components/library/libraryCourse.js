1b72ac6f8b5e17d20c5b9aa439b5ad3a4b12d09d
Matt Farnsworth <mryanfarnsworth@gmail.com>
2018-07-03 09:28:25 -0600

    Built library course component with grid and library grid.

---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library-course__title">Problem Solving</label>
+                {/* icon component */}
+                {/* arrow component */}
+                {/* action button component */}
+                <div className="library-course__description">
+                    <label>Couse Description</label>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type 
+                        specimen book. 
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
+export default LibraryCourse;
